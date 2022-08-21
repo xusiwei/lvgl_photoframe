ec1274fcc3268ea6dd8f9f071ed11524d538a8a3
Siwei Xu <xusiwei1236@qq.com>
2022-08-21 21:08:35 +0800

    Create project and enable LVGL demos

---
+++ applications/thumbnail.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-08-09     Administrator       the first version
+ */
+
---
+++ applications/thumbnail.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-08-09     xu       the first version
+ */
+#ifndef APPLICATIONS_THUMBNAIL_H_
+#define APPLICATIONS_THUMBNAIL_H_
+
+
+
+#endif /* APPLICATIONS_THUMBNAIL_H_ */
